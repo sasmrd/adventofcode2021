cdb6366781e7c88f5eade9d493c35bbdf7704926
sasmrd <sasmrd@live.com>
2020-12-10 17:53:51 +0000

    update day 10

--- src/task10.py
+++ src/task10.py
@@ -1,40 +1,5 @@
-from src.input_reader import InputReader
-
-
-class Graph:
-
-    def __init__(self, edges):
-        self.edges = edges
-        self.graph_dict = self.get_graph_dict()
-
-    def get_graph_dict(self):
-        graph_dict = {}
-        for start, end in self.edges:
-            if start in graph_dict:
-                graph_dict[start].append(end)
-            else:
-                graph_dict[start] = [end]
-        return graph_dict
-
-    def get_paths(self, start, end, path=[]):
-        path = path + [start]
-
-        if start == end:
-            return [path]
-
-        if start not in self.graph_dict:
-            return []
-
-        paths = []
-        for node in self.graph_dict[start]:
-            if node not in path:
-                new_paths = self.get_paths(node, end , path)
-                for p in new_paths:
-                    paths.append(p)
-
-        return paths
-
-
+import networkx as nx
+from input_reader import InputReader
 
 
 class Day10:
@@ -48,7 +13,6 @@ class Day10:
         three_jolt = 0
         for index, adapter in enumerate(input_list):
             try:
-                # print(adapter, input_list[index + 1], "diff =", input_list[index + 1] - adapter)
                 if input_list[index+1] - adapter == 1:
                     one_jolt += 1
                 elif input_list[index+1] - adapter == 3:
@@ -58,33 +22,59 @@ class Day10:
 
     def part_2(self):
         input_list = self.get_input_list()
-        routes = self.get_routes(input_list)
-        route_graph = Graph(routes)
-        all_paths = route_graph.get_paths(min(input_list), max(input_list))
-        # print(all_paths)
-        return len(all_paths)
+        routes, splits = self.get_routes(input_list)
+        ranges = self.ranges(splits)
+
+        prod = 1
+        for each in ranges:
+            g = nx.Graph()
+            this_range = input_list[input_list.index(each[0]): input_list.index(each[1])+1]
+            small_route = self.get_routes(this_range)[0]
+            g.add_edges_from(small_route)
+            paths = []
+            for path in nx.all_simple_paths(g, source=each[0], target=each[1]):
+                path.sort()
+                paths.append(path)
+            set_paths = set(tuple(row) for row in paths)
+
+            prod *= len(set_paths)
+        return prod
 
     def get_input_list(self):
         input_list = self.data.copy()
         input_list.sort(key=int)
         input_list.append(max(input_list)+3)
-        print(input_list)
         return input_list
 
     @staticmethod
     def get_routes(input_list):
         routes = []
+        splits = []
         for node in input_list:
+            next_steps = [node]
             if (node+1) in input_list:
                 routes.append((node, node+1))
+                next_steps.append(node+1)
             if (node+2) in input_list:
                 routes.append((node, node + 2))
+                next_steps.append(node+2)
             if (node+3) in input_list:
                 routes.append((node, node + 3))
-        return routes
+                next_steps.append(node+3)
+            if len(next_steps) > 2:
+                splits.extend(next_steps)
+        splits = list(dict.fromkeys(splits))
+        return routes, splits
+
+    @staticmethod
+    def ranges(nums):
+        nums = sorted(set(nums))
+        gaps = [[s, e] for s, e in zip(nums, nums[1:]) if s + 1 < e]
+        edges = iter(nums[:1] + sum(gaps, []) + nums[-1:])
+        return list(zip(edges, edges))
 
 
 if __name__ == '__main__':
-    day10 = Day10('inputs/task10t.txt')
-    print("Part 1:", day10.part_1(), "\n")
-    print("Part 2:", day10.part_2(), "\n")
+    day10 = Day10('inputs/task10.txt')
+    print("Part 1:", day10.part_1())
+    print("Part 2:", day10.part_2())
