eaaaa89b081c41b9ba54b7a2eca8df30e4ba3c97
sasmrd <sasmrd@live.com>
2020-12-15 11:00:01 +0000

    add day 15 and update input reader

--- src/input_reader.py
+++ src/input_reader.py
@@ -16,6 +16,13 @@ class InputReader:
             return [list(x.strip()) for x in f.readlines()]
 
     @staticmethod
+    def input_reader_one_line_int_list(file_name):
+        with open(file_name, "r") as f:
+            line = f.readline()
+            lst = line.split(",")
+            return [int(x) for x in lst]
+
+    @staticmethod
     def input_reader_string_blank_lines(file_name):
         with open(file_name, "r") as f:
             temp_list = [x.strip() for x in f.readlines()]
