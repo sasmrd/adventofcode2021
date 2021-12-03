ed538085518613f7d9d8447ad0071b52439444ac
sasmrd <sasmrd@live.com>
2021-12-03 13:49:35 +0000

    refactor

--- task1.py
+++ task1.py
@@ -7,23 +7,10 @@ class Day1:
         self.data = InputReader.input_reader_int(file)
 
     def part_1(self):
-        count = 0
-        before = 100000
-        for depth in self.data:
-            if depth > before:
-                count += 1
-            before = depth
-        return count
+        return sum(1 if self.data[i + 1] > self.data[i] else 0 for i in range(0, len(self.data)-1))
 
     def part_2(self):
-        count = 0
-        prev = 100000000
-        for i in range(0, len(self.data) - 2):
-            total = self.data[i] + self.data[i + 1] + self.data[i + 2]
-            if total > prev:
-                count += 1
-            prev = total
-        return count
+        return sum(1 if sum(self.data[i+1:i+4]) > sum(self.data[i:i+3]) else 0 for i in range(0, len(self.data)-3))
 
 
 if __name__ == '__main__':
