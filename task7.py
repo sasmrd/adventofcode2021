a330c53454f234aa50cebad276c666976525f886
sasmrd <sasmrd@live.com>
2021-12-07 09:51:41 +0000

    add task 7

---
+++ task7.py
@@ -0,0 +1,22 @@
+import math
+
+from input_reader import InputReader
+
+
+class Day7:
+
+    def __init__(self, file):
+        self.data = InputReader.input_reader_one_line_int_list(file)
+
+    def part_1(self):
+        return min(sum(abs(fish - value) for fish in self.data) for value in range(0, max(self.data)))
+
+    def part_2(self):
+        return min(sum(0.5*abs(fish - value)*(abs(fish - value)+1) for fish in self.data) for value in range(0, max(self.data)))
+
+
+if __name__ == '__main__':
+    day7 = Day7('inputs/task7.txt')
+    print(day7.data)
+    print(day7.part_1())
+    print(day7.part_2())
